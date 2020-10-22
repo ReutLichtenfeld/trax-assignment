6ab777cb06a25f4561e0ecfbc8bb0418df6ac100
ReutLichtenfeld <reutlichtenfeld@gmail.com>
2020-10-22 22:41:25 +0300

    Created a route for getting all the contacts as an array

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/api/whos-there', (req, res) => {
     res.send( 'Hi Trax! This is Reut Lichtenfeld :)' )
 });
 
+let contacts = new Map();
+
+app.get('/api/contacts', (req, res) => {
+    let contactsVal = [];
+    contacts.forEach(val => contactsVal.push(val));
+    res.send( contactsVal );
+});
+
 //START SERVER
 const port = 3000;
 
