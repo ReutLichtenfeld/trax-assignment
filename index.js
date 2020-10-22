4a22682c6070ec980a9862815a15d0d7af10675f
ReutLichtenfeld <reutlichtenfeld@gmail.com>
2020-10-22 22:32:30 +0300

    Created the first route GET /whos-there route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api/whos-there', (req, res) => {
+    res.send( 'Hi Trax! This is Reut Lichtenfeld :)' )
+});
+
 //START SERVER
 const port = 3000;
 
